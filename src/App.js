78a40406cc597552cc270e7a3e8614501649e75f
Abdalrhman Atef <abdalrhman.atef.ramadan@gmail.com>
2022-10-22 20:51:04 +0200

    modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,77 @@
-import Form from "./components/Form";
 import Todo from "./components/Todo";
 import { useTodos } from "./context/todoContext";
-
+import { IoMdAdd } from "react-icons/io";
 function App() {
   const { todos, todoscount } = useTodos();
+  const {
+    mood,
+    setMood,
+    inputRef,
+    addTodo,
+    todo,
+    inputValue,
+    setInputValue,
+    updateTodo,
+    theId,
+  } = useTodos();
+
+  window.onkeyup = (e) => {
+    if (e.key === "Enter") {
+      if (inputValue !== "") {
+        if (mood === "add") {
+          addTodo({
+            ...todo,
+            id: new Date().getTime().toString(),
+            title: inputValue,
+          });
+        } else {
+          updateTodo(theId, inputValue);
+        }
+        inputRef.current.value = "";
+        setInputValue("");
+        inputRef.current.focus();
+        setMood("add");
+      }
+    }
+  };
 
   return (
     <div className={`app`}>
       <div className="title">
         <h1>Todo App</h1>
-        <Form />
+        <div className="form">
+          <div className="inputs">
+            <input
+              type="text"
+              placeholder="Add Task"
+              onChange={(e) => {
+                setInputValue(e.target.value);
+              }}
+              ref={inputRef}
+            />
+            <button
+              onClick={() => {
+                if (inputValue !== "") {
+                  if (mood === "add") {
+                    addTodo({
+                      ...todo,
+                      id: new Date().getTime().toString(),
+                      title: inputValue,
+                    });
+                  } else {
+                    updateTodo(theId, inputValue);
+                  }
+                }
+                inputRef.current.value = "";
+                setInputValue("");
+                inputRef.current.focus();
+                setMood("add");
+              }}
+            >
+              <IoMdAdd size={30} />
+            </button>
+          </div>
+        </div>
       </div>
       <div className="todos-container">
         {todos?.map((todo) => (
